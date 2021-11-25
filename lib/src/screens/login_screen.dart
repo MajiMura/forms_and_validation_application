bfb48c3efe776765c554a170be548b9638e1c445
Joshua Green <joshuagreen@Joshuas-MacBook-Pro.local>
2021-11-25 17:04:25 +0000

    feat: Created app boilerplate code.

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(20.0),
+      child: const Text('Login Screen'),
+    );
+  }
+}
